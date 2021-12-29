9f719dc925f36f1d3fc0cc53cb871a19370d64ed
sidm9 <siddharthm1999@outlook.com>
2021-12-29 12:44:01 +0530

    added Enviornment Variables

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
+mod config;
 mod models;
 use crate::models::Status;
 use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
 use std::io;
 
-async fn status() -> impl Responder{
-    web::HttpResponse::Ok().json(Status {status: "Ok".to_string()})
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok().json(Status {
+        status: "Ok".to_string(),
+    })
 }
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(status))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    dotenv().ok();
+
+    let config = config::Config::from_env().unwrap();
+
+    println!(
+        "Server Starting at https://{}:{}",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
+        .run()
+        .await
 }
